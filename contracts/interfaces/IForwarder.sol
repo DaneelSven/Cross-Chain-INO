7828372ff4afc1342b426277fe8eaf81804d3cfc
Sven Daneel <sven4696@gmail.com>
2024-06-21 22:34:22 +0200

    adding trusted forwarder and interfaces

---
+++ contracts/interfaces/IForwarder.sol
@@ -0,0 +1,28 @@
+interface IForwarder {
+    struct ForwardRequest {
+        address from;
+        address to;
+        uint256 value;
+        uint256 gas;
+        uint256 nonce;
+        bytes data;
+    }
+
+    function getNonce(address from) external view returns (uint256);
+    function verify(
+        ForwardRequest calldata req,
+        bytes32 domainSeparator,
+        bytes32 requestTypeHash,
+        bytes calldata suffixData,
+        bytes calldata sig
+    ) external view;
+    function execute(
+        ForwardRequest calldata req,
+        bytes32 domainSeparator,
+        bytes32 requestTypeHash,
+        bytes calldata suffixData,
+        bytes calldata sig
+    ) external payable returns (bool, bytes memory);
+    function registerRequestType(string calldata typeName, string calldata typeSuffix) external;
+    function registerDomainSeparator(string calldata name, string calldata version) external;
+}
